8d3abe2d7c274f3d1a8930611d215021e32a8813
sushakov <sergey.ushakov94@gmail.com>
2016-12-02 20:03:09 +0200

    fix UserTask.js helpers

--- client/layouts/tasks/user/UserTask.js
+++ client/layouts/tasks/user/UserTask.js
@@ -35,6 +35,32 @@ Template.UserTask.helpers({
     task: () => {
         return tasks.findOne({_id: FlowRouter.getParam('taskId')});
     },
+    desc: (text) => {
+        var userTask = usertask.findOne({taskId: FlowRouter.getParam('taskId')});
+        var vars = variables.find({task: FlowRouter.getParam('taskId'), user: Meteor.userId()}).fetch();
+        var globalVars = variables.find({task: null, user: Meteor.userId()}).fetch();
+
+        const regEx = /&lt;v&gt;(.+?)&lt;\/v&gt;/g;
+        let regExProfile = /profile\s(.*)/;
+        let regExGlobal = /global\s(.*)/;
+        let match;
+
+        return text.replace(regEx, function(s, key) {
+            if(match = regExProfile.exec(key))
+                return Meteor.user().profile[match[1]];
+            else if(match = regExGlobal.exec(key)){
+                for(_var of globalVars)
+                    if(_var.name == match[0])
+                        return _var.value;
+            }
+            else{
+                for(Var of vars){
+                    if(Var.name == key)
+                        return Var.value;
+                }
+            }
+        });
+    }
 });
 
 
